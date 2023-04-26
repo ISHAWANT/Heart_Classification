8b2872cef23965616dbaaec2b019a62182509349
ishawant kumar <ishawantkumar@gmail.com>
2023-04-26 17:07:18 +0530

    execption file done

---
+++ HeartClassification/exception/__init__.py
@@ -0,0 +1,27 @@
+import os,sys 
+
+class HeartException(Exception):
+    
+    def __init__(self,error_message:Exception,error_details:sys):
+        super().__init__(error_message) 
+        self.error_message= HeartException.get_detailed_error_message(error_message=error_message,error_details=error_details) 
+        
+    @staticmethod 
+    def get_detailed_error_message(error_message:Exception,error_details:sys)->str:
+        
+        _,_,exec_tb = error_details.exc_info() 
+        exception_block_line_number = exec_tb.tb_frame.f_lineno 
+        try_block_line_number = exec_tb.tb_lineno 
+        file_name = exec_tb.tb_frame.f_code.co_filename
+        error_message = f"""
+        Error occured in script:
+        [ {file_name} ] at
+        try block line_number: [{try_block_line_number}] and exception block line number: [{exception_block_line_number}]
+        error message: [{error_message}] 
+        """
+        return error_message 
+    def __str__(self):
+        return self.error_message 
+    
+    def __repr__(self) -> str:
+        return HeartException.__name__.str() 
